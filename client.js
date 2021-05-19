91b6f057965441502ba012d235956720c95b6e2c
Simone Odegard <Simone.Odegard@gmail.com>
2021-05-19 16:15:22 -0700

    database added and working

--- client.js
+++ client.js
@@ -4,14 +4,14 @@ const socket = require('socket.io-client')('http://localhost:3000');
 const repl = require('repl');
 const chalk = require('chalk');
 const { argv, emit } = require('process');
-const { io } = require('socket.io-client');
+// const messages = document.getElementById('messages');
+// const msgForm = document.getElementById('msgForm');
 let username = null;
 
-
 socket.on('disconnect', () => {
-  socket.emit('disconnect');
+  console.log('disconnected');
+  // socket.emit('disconnect');
 })
-
 
 socket.on('connect', () => {
   console.log(chalk.red('---- Start Chatting ----'))
@@ -22,18 +22,31 @@ socket.on('connect', () => {
 socket.on('message', (data) => {
   const { User_Message, username } = data;
   console.log(chalk.green(username + ':' + User_Message.split('\n')[0]));
+  socket.emit('chat message', data)
 })
 
 socket.on('private message', (data) => {
   const { User_Message, username } = data;
-  console.log(username)
   console.log(chalk.green(username + ':' + User_Message.split('\n')[0]));
+  socket.emit('chat message', data)
+});
+
+socket.on('message list', (data) => {
+  data.allMessages.forEach((chatMessage) => {
+    const { User_Message, username } = chatMessage;
+    if (!chatMessage.privateReceiver) {
+      console.log(chalk.green(username + ':' + User_Message.split('\n')[0]));
+    } else {
+      if (username === data.currentUser || data.currentUser === chatMessage.privateReceiver) {
+        console.log(chalk.green(username + ':' + User_Message.split('\n')[0]));
+      }
+    }
+  })
 });
 
 repl.start({
   prompt: '',
   eval: (User_Message) => {
- // add a conditional for regular message or private message
 
     let regex1 = /(\S+\w+\s+){2}/gm; //grabs /w Jason
     let regex1string = User_Message.match(regex1);
@@ -51,10 +64,7 @@ repl.start({
     // console.log('regex1string---', regex1string);
     // console.log('messageType---', messageType);
     // console.log('privateReceiver---', privateReceiver);
-    console.log('sender username ---', username);
-
-    
-
+    // console.log('sender username ---', username);
 
     if(messageType === '/to'){
       socket.emit('private message', { username, User_Message, privateReceiver, messageType })
